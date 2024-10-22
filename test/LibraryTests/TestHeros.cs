5c13c89b8eff028c7be22d79136dd3b783fee4a5
Jessica-Mora <mora36702@hotmail.com>
2024-10-21 23:37:26 -0300

    test2

--- test/LibraryTests/TestHeros.cs
+++ test/LibraryTests/TestHeros.cs
@@ -1,6 +1,30 @@
+using Library;
+using NUnit.Framework;
+using Ucu.Poo.RoleplayGame;
+
 namespace LibraryTests;
 
 public class TestHeros
 {
-    
+    public void Setup()
+    {
+    }
+    [Test]
+    public void create_BaseCharacter() //Crea la base del personaje hero
+    {
+        BaseCharacter character = new BaseCharacter("test");
+        
+        Assert.That("test",Is.EqualTo(character.Name));
+        Assert.That(100, Is.EqualTo(character.Health));
+        Assert.That(0, Is.EqualTo(character.Vp));
+    }
+
+    [Test]
+    public void Cure_character()
+    {
+        BaseCharacter character = new BaseCharacter("test");
+        character.ReceiveAttack(50);
+        character.Cure();
+        Assert.That(100, Is.EqualTo(character.Health));
+    }
 }
